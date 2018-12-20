1d7b363ddb3fdd623cc47196c447f3d7936deb92
Biggee <lucloner@hotmail.com>
2018-12-21 06:44:44 +0800

    camera2sample shortened

--- MainActivity/src/main/java/cn/biggee/hardware/camera2/MainActivity/MainActivity.java
+++ MainActivity/src/main/java/cn/biggee/hardware/camera2/MainActivity/MainActivity.java
@@ -15,11 +15,8 @@ import android.os.HandlerThread;
 import android.support.constraint.ConstraintLayout;
 import android.view.Surface;
 import android.view.TextureView;
-import android.widget.Toast;
 
 import java.util.Collections;
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends Activity {
 
@@ -33,7 +30,6 @@ public class MainActivity extends Activity {
 
             final TextureView mTextureView = new TextureView(mainActivity);
             ((ConstraintLayout) findViewById(R.id.mLayout)).addView(mTextureView);
-            final Semaphore mCameraOpenCloseLock = new Semaphore(1);
 
             final int STATE_PREVIEW = 0;
             final int STATE_WAITING_LOCK = 1;
@@ -51,59 +47,10 @@ public class MainActivity extends Activity {
 
             final CameraManager manager = (CameraManager) mainActivity.getSystemService(Context.CAMERA_SERVICE);
 
-            while (!mCameraOpenCloseLock.tryAcquire(2500, TimeUnit.MILLISECONDS)) {
-                Toast.makeText(mainActivity, "Time out waiting to lock camera opening.", Toast.LENGTH_SHORT).show();
-            }
-
-            final CameraCaptureSession.CaptureCallback mCaptureCallback = new CameraCaptureSession.CaptureCallback() {
-                private void process(CaptureResult result) {
-                    switch ((int) objects[1]) {
-                        case STATE_PREVIEW: {
-                            // We have nothing to do when the camera preview is working normally.
-                            break;
-                        }
-                        case STATE_WAITING_LOCK: {
-                            Integer afState = result.get(CaptureResult.CONTROL_AF_STATE);
-                            if (afState == null) {
-                                //captureStillPicture();
-                            } else if (CaptureResult.CONTROL_AF_STATE_FOCUSED_LOCKED == afState || CaptureResult.CONTROL_AF_STATE_NOT_FOCUSED_LOCKED == afState) {
-                                // CONTROL_AE_STATE can be null on some devices
-                                Integer aeState = result.get(CaptureResult.CONTROL_AE_STATE);
-                                if (aeState == null || aeState == CaptureResult.CONTROL_AE_STATE_CONVERGED) {
-                                    objects[1] = STATE_PICTURE_TAKEN;
-                                    //captureStillPicture();
-                                } else {
-                                    //runPrecaptureSequence();
-                                }
-                            }
-                            break;
-                        }
-                        case STATE_WAITING_PRECAPTURE: {
-                            // CONTROL_AE_STATE can be null on some devices
-                            Integer aeState = result.get(CaptureResult.CONTROL_AE_STATE);
-                            if (aeState == null || aeState == CaptureResult.CONTROL_AE_STATE_PRECAPTURE || aeState == CaptureRequest.CONTROL_AE_STATE_FLASH_REQUIRED) {
-                                objects[1] = STATE_WAITING_NON_PRECAPTURE;
-                            }
-                            break;
-                        }
-                        case STATE_WAITING_NON_PRECAPTURE: {
-                            // CONTROL_AE_STATE can be null on some devices
-                            Integer aeState = result.get(CaptureResult.CONTROL_AE_STATE);
-                            if (aeState == null || aeState != CaptureResult.CONTROL_AE_STATE_PRECAPTURE) {
-                                objects[1] = STATE_PICTURE_TAKEN;
-                                //captureStillPicture();
-                            }
-                            break;
-                        }
-                    }
-                }
-            };
-
             final CameraDevice.StateCallback mStateCallback = new CameraDevice.StateCallback() {
                 @Override
                 public void onOpened(CameraDevice cameraDevice) {
                     // This method is called when the camera is opened.  We start camera preview here.
-                    mCameraOpenCloseLock.release();
                     objects[0] = cameraDevice;
                     try {
                         SurfaceTexture texture = mTextureView.getSurfaceTexture();
@@ -133,7 +80,50 @@ public class MainActivity extends Activity {
                                             //setAutoFlash(mPreviewRequestBuilder);
                                             // Finally, we start displaying the camera preview.
                                             final CaptureRequest mPreviewRequest = mPreviewRequestBuilder.build();
-                                            cameraCaptureSession.setRepeatingRequest(mPreviewRequest, mCaptureCallback, mBackgroundHandler);
+                                            cameraCaptureSession.setRepeatingRequest(mPreviewRequest, new CameraCaptureSession.CaptureCallback() {
+                                                        private void process(CaptureResult result) {
+                                                            switch ((int) objects[1]) {
+                                                                case STATE_PREVIEW: {
+                                                                    // We have nothing to do when the camera preview is working normally.
+                                                                    break;
+                                                                }
+                                                                case STATE_WAITING_LOCK: {
+                                                                    Integer afState = result.get(CaptureResult.CONTROL_AF_STATE);
+                                                                    if (afState == null) {
+                                                                        //captureStillPicture();
+                                                                    } else if (CaptureResult.CONTROL_AF_STATE_FOCUSED_LOCKED == afState || CaptureResult.CONTROL_AF_STATE_NOT_FOCUSED_LOCKED == afState) {
+                                                                        // CONTROL_AE_STATE can be null on some devices
+                                                                        Integer aeState = result.get(CaptureResult.CONTROL_AE_STATE);
+                                                                        if (aeState == null || aeState == CaptureResult.CONTROL_AE_STATE_CONVERGED) {
+                                                                            objects[1] = STATE_PICTURE_TAKEN;
+                                                                            //captureStillPicture();
+                                                                        } else {
+                                                                            //runPrecaptureSequence();
+                                                                        }
+                                                                    }
+                                                                    break;
+                                                                }
+                                                                case STATE_WAITING_PRECAPTURE: {
+                                                                    // CONTROL_AE_STATE can be null on some devices
+                                                                    Integer aeState = result.get(CaptureResult.CONTROL_AE_STATE);
+                                                                    if (aeState == null || aeState == CaptureResult.CONTROL_AE_STATE_PRECAPTURE || aeState == CaptureRequest.CONTROL_AE_STATE_FLASH_REQUIRED) {
+                                                                        objects[1] = STATE_WAITING_NON_PRECAPTURE;
+                                                                    }
+                                                                    break;
+                                                                }
+                                                                case STATE_WAITING_NON_PRECAPTURE: {
+                                                                    // CONTROL_AE_STATE can be null on some devices
+                                                                    Integer aeState = result.get(CaptureResult.CONTROL_AE_STATE);
+                                                                    if (aeState == null || aeState != CaptureResult.CONTROL_AE_STATE_PRECAPTURE) {
+                                                                        objects[1] = STATE_PICTURE_TAKEN;
+                                                                        //captureStillPicture();
+                                                                    }
+                                                                    break;
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                    , mBackgroundHandler);
                                         } catch (CameraAccessException e) {
                                             e.printStackTrace();
                                         }
@@ -153,45 +143,18 @@ public class MainActivity extends Activity {
 
                 @Override
                 public void onDisconnected(CameraDevice cameraDevice) {
-                    mCameraOpenCloseLock.release();
                     cameraDevice.close();
                     objects[0] = null;
                 }
 
                 @Override
                 public void onError(CameraDevice cameraDevice, int error) {
-                    mCameraOpenCloseLock.release();
                     cameraDevice.close();
                     objects[0] = null;
                     mainActivity.finish();
                 }
             };
 
-            final TextureView.SurfaceTextureListener mSurfaceTextureListener = new TextureView.SurfaceTextureListener() {
-                @Override
-                public void onSurfaceTextureAvailable(SurfaceTexture texture, int width, int height) {
-                    try {
-                        assert manager != null;
-                        manager.openCamera(manager.getCameraIdList()[0], mStateCallback, mBackgroundHandler);
-                    } catch (SecurityException | CameraAccessException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                @Override
-                public void onSurfaceTextureSizeChanged(SurfaceTexture texture, int width, int height) {
-                    //configureTransform(width, height);
-                }
-
-                @Override
-                public boolean onSurfaceTextureDestroyed(SurfaceTexture texture) {
-                    return true;
-                }
-
-                @Override
-                public void onSurfaceTextureUpdated(SurfaceTexture texture) {
-                }
-            };
 
             if (mTextureView.isAvailable()) {
                 try {
@@ -201,7 +164,31 @@ public class MainActivity extends Activity {
                     e.printStackTrace();
                 }
             } else {
-                mTextureView.setSurfaceTextureListener(mSurfaceTextureListener);
+                mTextureView.setSurfaceTextureListener(new TextureView.SurfaceTextureListener() {
+                    @Override
+                    public void onSurfaceTextureAvailable(SurfaceTexture texture, int width, int height) {
+                        try {
+                            assert manager != null;
+                            manager.openCamera(manager.getCameraIdList()[0], mStateCallback, mBackgroundHandler);
+                        } catch (SecurityException | CameraAccessException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onSurfaceTextureSizeChanged(SurfaceTexture texture, int width, int height) {
+                        //configureTransform(width, height);
+                    }
+
+                    @Override
+                    public boolean onSurfaceTextureDestroyed(SurfaceTexture texture) {
+                        return true;
+                    }
+
+                    @Override
+                    public void onSurfaceTextureUpdated(SurfaceTexture texture) {
+                    }
+                });
             }
         } catch (Exception e) {
             e.printStackTrace();
